c2d6b12043f970e67cbc428914cca82f7b9693fa
Alex Zalo <zalo.alex56@gmail.com>
2024-11-20 18:14:36 +0100

    Change icons and update extension list

--- src/popup/components/extensions.tsx
+++ src/popup/components/extensions.tsx
@@ -17,26 +17,37 @@ function ExtensionCard({ name, icon, showcase, github, install }: Extension) {
         </div>
         <div className="flex gap-3 items-center">
             { showcase && <a href={showcase} className="text-sm">Showcase</a> }
-            { github && <a href="#" className="text-sm">Github</a>}
+            { github && <a href={github} className="text-sm">Github</a>}
             <div className="h-full w-[1px] bg-gray-300"></div>
-            <a href={install} className="p-2 px-3 rounded bg-green-700 text-white">Install</a>
+            {
+                install ?
+                    <a href={install} className="p-2 px-3 rounded bg-green-700 text-white">Install</a>
+                : <span className="p-2 px-3 text-gray-400">Installed</span>
+            }
         </div>
     </div>
 }
 
 export default function Extensions() {
 
-    const [extensions, setExtensions] = useState<Extension[]>([])
+    const [extensions, setExtensions] = useState<Extension[]>([
+        {
+            "name": "Discord Web",
+            "icon": "icons/discord.png",
+            "install": "https://udes.app/",
+            "github": "https://github.com/UDES-Project/discord-client"
+        }
+    ])
 
     useEffect(() => {
-        UDES_Popup.getExtensions((ext: Extension) => setExtensions([...extensions, ext]))
+        UDES_Popup.getExtensions((ext: Extension) => setExtensions([ext, ...extensions]))
     }, [])
 
     return <div>
         <div className="flex flex-col gap-2">
             {
                 extensions.map((ext) => {
-                    return <ExtensionCard key={ext.name} name={ext.name} icon={ext.icon}/>
+                    return <ExtensionCard key={ext.name} name={ext.name} icon={ext.icon} showcase={ext.showcase} github={ext.github} install={ext.install}/>
                 })
             }
         </div>
--- src/popup/components/home.tsx
+++ src/popup/components/home.tsx
@@ -3,8 +3,8 @@ export default function Home() {
         <div className="mt-20 flex flex-col justify-center items-center">
             <h1 className="text-center text-4xl font-bold text-green-700 mb-5">UDES</h1>
             <div className="flex gap-2">
-                <a href="#" className="px-4 py-2 bg-green-700 text-white rounded">Website</a>
-                <a href="#" className="px-4 py-2 bg-green-700 text-white rounded">Github</a>
+                <a href="https://udes.app" className="px-4 py-2 bg-green-700 text-white rounded">Website</a>
+                <a href="https://github.com/UDES-Project" className="px-4 py-2 bg-green-700 text-white rounded">Github</a>
             </div>
         </div>
     </div>
